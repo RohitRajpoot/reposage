35616759dcc805d8f9c78c59dc502749051de4ed
Rohit Rajpoot <rohitrajpoot@Rohits-MacBook-Pro.local>
2025-06-21 14:30:07 -0400

    Fix imports in app.py: import embed_chat & bayes_chat

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
-from assist.chat import chat as chat_plugin
+from assist.chat import chat as embed_chat
+from assist.bayes_chat import bayes_chat
 
 st.title("RepoSage Chatbot Demo")
 
-# 1) Change the label to make it obvious we're asking a question
-question = st.text_input("Ask RepoSage a question:", "")
+question = st.text_input("Enter your question below:")
 
 col1, col2 = st.columns(2)
 with col1:
     if st.button("Ask Embedding RepoSage"):
-        st.write(embed_chat(question))
+        answer = embed_chat(question)
+        st.write(answer)
 
 with col2:
     if st.button("Ask Bayesian RepoSage"):
-        st.write(bayes_chat(question))
+        answer = bayes_chat(question)
+        st.write(answer)
